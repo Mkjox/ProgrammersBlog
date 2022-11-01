6fa2a9d3a15434a9b10b70e6216b16d24cadfa96
Mert <zincir_890@hotmail.com>
2022-11-01 23:51:04 +0300

    Created Migrate and Changed Services

--- ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/CategoryMap.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Mappings/CategoryMap.cs
@@ -14,7 +14,7 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Mappings
         public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.HasKey(c => c.Id);
-            builder.Property(c => c).ValueGeneratedOnAdd();
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
             builder.Property(c => c.Name).IsRequired();
             builder.Property(c => c.Name).HasMaxLength(70);
             builder.Property(c => c.Description).HasMaxLength(500);
--- ProgrammersBlog.Data/Concrete/UnitOfWork.cs
+++ ProgrammersBlog.Data/Concrete/UnitOfWork.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ProgrammersBlog.Data.Concrete
 {
     public class UnitOfWork : IUnitOfWork
