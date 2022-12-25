eaf4bada96cb386749e48fd79258410cc704d0fb
Mert <zincir_890@hotmail.com>
2022-12-25 23:47:26 +0300

    #59 Refactor Refresh and Delete in FrontEnd

--- ProgrammersBlog.Mvc/wwwroot/js/site.js
+++ ProgrammersBlog.Mvc/wwwroot/js/site.js
@@ -1,7 +1,8 @@
 ﻿function convertFirstLetterToUpperCase(text) {
     return text.charAt(0).toUpperCase() + text.slice(1);
 }
+
 function convertToShortDate(dateString) {
-    const shortDate = new Date(dateString).toLocalDateString('en-US');
+    const shortDate = new Date(dateString).toLocaleDateString();
     return shortDate;
 }
