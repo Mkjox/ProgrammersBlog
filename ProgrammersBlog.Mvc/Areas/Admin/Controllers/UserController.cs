a2ccb54665517813c97936dbaefa73e87561550c
jox <zincir_890@hotmail.com>
2023-04-20 14:12:10 +0300

    Added ImageDelete

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -154,5 +154,20 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             }
             return fileName;
         }
+
+        public bool ImageDelete(string pictureName)
+        {
+            string wwwroot = _env.WebRootPath;
+            var fileToDelete = Path.Combine($"{wwwroot}/img", pictureName);
+            if (System.IO.File.Exists(fileToDelete))
+            {
+                System.IO.File.Delete(fileToDelete);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
