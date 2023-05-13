ce3ea910b812b5843bf18d3b7546f1410a98679d
jox <zincir_890@hotmail.com>
2023-05-13 23:57:59 +0300

    Completed UploadUserImage Method

---
+++ ProgrammersBlog.Mvc/Helpers/Concrete/ImageHelper.cs
@@ -0,0 +1,49 @@
+﻿using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Mvc.Helpers.Abstract;
+using ProgrammersBlog.Shared.Utilities.Extensions;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using ProgrammersBlog.Shared.Utilities.Results.Concrete;
+
+namespace ProgrammersBlog.Mvc.Helpers.Concrete
+{
+    public class ImageHelper : IImageHelper
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly string _wwwroot;
+        private readonly string imgFolder = "img";
+
+        public ImageHelper(IWebHostEnvironment env)
+        {
+            _env = env;
+            _wwwroot = _env.WebRootPath;
+        }
+
+        public async Task<IDataResult<UploadedImageDto>> UploadUserImage(string userName, IFormFile pictureFile, string folderName = "userImages")
+        {
+            if (!Directory.Exists($"{_wwwroot}/{imgFolder}/{folderName}"))
+            {
+                Directory.CreateDirectory($"{_wwwroot}/{imgFolder}/{folderName}");
+            }
+
+            string oldFileName = Path.GetFileNameWithoutExtension(pictureFile.FileName);
+            string fileExtension = Path.GetExtension(pictureFile.FileName);
+            DateTime dateTime = DateTime.Now;
+            string newFileName = $"{userName}_{dateTime.FullDateAndTimeStringWithUnderscore()}{fileExtension}";
+            var path = Path.Combine($"{_wwwroot}/{imgFolder}/{folderName}", newFileName);
+            await using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await pictureFile.CopyToAsync(stream);
+            }
+            return new DataResult<UploadedImageDto>(ResultStatus.Success, $"{userName} adlı kullanıcının resmi başarıyla yüklenmiştir.", new UploadedImageDto
+            {
+                FullName = $"{folderName}/{newFileName}",
+                OldName = oldFileName,
+                Extension = fileExtension,
+                FolderName = folderName,
+                Path = path,
+                Size = pictureFile.Length
+            });
+        }
+    }
+}
