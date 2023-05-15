e123ca792eea0cf50ca7059addc61628a6d82b97
jox <zincir_890@hotmail.com>
2023-05-16 01:29:06 +0300

    Created ImageDeletedDto Class and Delete Method

--- ProgrammersBlog.Mvc/Helpers/Concrete/ImageHelper.cs
+++ ProgrammersBlog.Mvc/Helpers/Concrete/ImageHelper.cs
@@ -19,6 +19,7 @@ namespace ProgrammersBlog.Mvc.Helpers.Concrete
             _wwwroot = _env.WebRootPath;
         }
 
+
         public async Task<IDataResult<UploadedImageDto>> UploadUserImage(string userName, IFormFile pictureFile, string folderName = "userImages")
         {
             if (!Directory.Exists($"{_wwwroot}/{imgFolder}/{folderName}"))
@@ -44,6 +45,29 @@ namespace ProgrammersBlog.Mvc.Helpers.Concrete
                 Path = path,
                 Size = pictureFile.Length
             });
+        }
+
+        public IDataResult<ImageDeletedDto> Delete(string pictureName)
+        {
+            var fileToDelete = Path.Combine($"{_wwwroot}/{imgFolder}/", pictureName);
+            if (System.IO.File.Exists(fileToDelete))
+            {
+                var fileInfo = new FileInfo(fileToDelete);
+                var imageDeletedDto = new ImageDeletedDto
+                {
+                    FullName = pictureName,
+                    Extension = fileInfo.Extension,
+                    Path = fileInfo.FullName,
+                    Size = fileInfo.Length
+                };
+
+                System.IO.File.Delete(fileToDelete);
+                return new DataResult<ImageDeletedDto>(ResultStatus.Success, imageDeletedDto);
+            }
+            else
+            {
+                return new DataResult<ImageDeletedDto>(ResultStatus.Error, $"Böyle bir resim bulunamadı.", null);
+            }
         }
     }
 }
