ce2778b43c2e94de786264d809ee7c88a897d579
jox <zincir_890@hotmail.com>
2023-06-07 15:08:42 +0300

    Created Article/Index page

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var result = await _articleService.GetAllByNonDeletedAsync();
+            if (result.ResultStatus == Shared.Utilities.Results.ComplexTypes.ResultStatus.Success) return View(result.Data);
+            return NotFound();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
