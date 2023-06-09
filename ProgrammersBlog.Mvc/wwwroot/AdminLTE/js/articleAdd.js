d6bfa599443ff6b602b4662618ed384d3e4d8868
jox <zincir_890@hotmail.com>
2023-06-09 20:10:09 +0300

    Added Trumbowyg for jQuery Text Editor

---
+++ ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('#text-editor').trumbowyg({
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['foreColor', 'backColor'],
+            ['emoji'],
+            ['fontfamily'],
+            ['fontsize']
+        ]
+    });
+});
