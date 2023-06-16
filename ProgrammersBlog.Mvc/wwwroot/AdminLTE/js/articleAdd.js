cb42292728c7851dc7c41301943a9370a9256832
jox <zincir_890@hotmail.com>
2023-06-16 20:33:02 +0300

    Added jQuery UI - DatePicker

--- ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
+++ ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
@@ -30,4 +30,30 @@
         placeholder: 'Lütfen bir kategori seçiniz...',
         allowClear: true,
     });
+
+    // jQuery UI - DatePicker
+    $(function () {
+        $("#datepicker").datepicker({
+            closeText: "kapat",
+            prevText: "geri",
+            nextText: "ileri",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            duration: 700,
+            minDate: -3,
+            maxDate: +3,
+        });
+    });
 });
