c84aea9a0e0db1529af58a91a7e850cffae8a61b
jox <zincir_890@hotmail.com>
2023-08-11 22:46:11 +0300

    Completed Assigning Roles

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/RoleController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/RoleController.cs
@@ -5,7 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProgrammersBlog.Entities.Concrete;
 using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
 using ProgrammersBlog.Mvc.Helpers.Abstract;
+using ProgrammersBlog.Shared.Utilities.Extensions;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 using System.Text.Json;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
@@ -15,7 +18,7 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
     {
         private readonly RoleManager<Role> _roleManager;
 
-        public RoleController(RoleManager<Role> roleManager,UserManager<User> userManager, IMapper mapper, IImageHelper imageHelper) : base(userManager, mapper, imageHelper)
+        public RoleController(RoleManager<Role> roleManager, UserManager<User> userManager, IMapper mapper, IImageHelper imageHelper) : base(userManager, mapper, imageHelper)
         {
             _roleManager = roleManager;
         }
@@ -47,7 +50,7 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> Assign(int userId)
         {
-            var user = await UserManager.Users.SingleOrDefaultAsync(u=>u.Id==userId);
+            var user = await UserManager.Users.SingleOrDefaultAsync(u => u.Id == userId);
             var roles = await _roleManager.Roles.ToListAsync();
             var userRoles = await UserManager.GetRolesAsync(user);
             UserRoleAssignDto userRoleAssignDto = new UserRoleAssignDto
@@ -66,6 +69,48 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
                 userRoleAssignDto.RoleAssignDtos.Add(roleAssignDto);
             }
             return PartialView("_RoleAssignPartial", userRoleAssignDto);
+        }
+
+        [Authorize(Roles = "SuperAdmin,User.Update")]
+        [HttpPost]
+        public async Task<IActionResult> Assign(UserRoleAssignDto userRoleAssignDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.Users.SingleOrDefaultAsync(u => u.Id == userRoleAssignDto.UserId);
+                foreach (var roleAssignDto in userRoleAssignDto.RoleAssignDtos)
+                {
+                    if (roleAssignDto.HasRole)
+                    {
+                        await UserManager.AddToRoleAsync(user, roleAssignDto.RoleName);
+                    }
+                    else
+                    {
+                        await UserManager.RemoveFromRoleAsync(user,roleAssignDto.RoleName);
+                    }
+                }
+
+                var userRoleAssignAjaxViewModel = JsonSerializer.Serialize(new UserRoleAssignAjaxViewModel
+                {
+                    UserDto = new UserDto
+                    {
+                        User = user,
+                        Message  = $"{user.UserName} kullanıcısına ait rol atama işlemi başarıyla tamamlanmıştır.",
+                        ResultStatus = ResultStatus.Success
+                    },
+                    RoleAssignPartial = await this.RenderViewToStringAsync("_RoleAssignPartial", userRoleAssignDto)
+                });
+                return Json(userRoleAssignAjaxViewModel);
+            }
+            else
+            {
+                var userRoleAssignAjaxErrorModel = JsonSerializer.Serialize(new UserRoleAssignAjaxViewModel
+                {
+                    RoleAssignPartial = await this.RenderViewToStringAsync("_RoleAssignPartial", userRoleAssignDto),
+                    UserRoleAssignDto = userRoleAssignDto
+                });
+                return Json(userRoleAssignAjaxErrorModel);
+            }
         }
     }
 }
