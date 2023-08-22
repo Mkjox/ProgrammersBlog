af19050d369056bbec875bc7700e60fde9a04b1e
jox <zincir_890@hotmail.com>
2023-08-22 04:38:57 +0300

    Optimized Uploading Picture / Deleting User

--- ProgrammersBlog.Mvc/Helpers/Concrete/ImageHelper.cs
+++ ProgrammersBlog.Mvc/Helpers/Concrete/ImageHelper.cs
@@ -5,6 +5,7 @@ using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.Abstract;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 using ProgrammersBlog.Shared.Utilities.Results.Concrete;
+using System.Text.RegularExpressions;
 
 namespace ProgrammersBlog.Mvc.Helpers.Concrete
 {
@@ -38,6 +39,9 @@ namespace ProgrammersBlog.Mvc.Helpers.Concrete
             string oldFileName = Path.GetFileNameWithoutExtension(pictureFile.FileName);
             /* Resimin uzantısı fileExtension adlı değişkene atanır. */
             string fileExtension = Path.GetExtension(pictureFile.FileName);
+
+            Regex regex = new Regex("[*'\",._&#^@]");
+            name = regex.Replace(name,string.Empty);
 
             DateTime dateTime = DateTime.Now;
 
