1706f7058855247b76e0e509705d60ada9126219
jox <zincir_890@hotmail.com>
2023-09-16 14:06:11 +0300

    Created ArticleController and Detail Action

--- ProgrammersBlog.Services/Concrete/ArticleManager.cs
+++ ProgrammersBlog.Services/Concrete/ArticleManager.cs
@@ -24,7 +24,7 @@ namespace ProgrammersBlog.Services.Concrete
 
         public async Task<IDataResult<ArticleDto>> GetAsync(int articleId)
         {
-            var article = await UnitOfWork.Articles.GetAsync(a => a.Id == articleId, a => a.User, a => a.Category);
+            var article = await UnitOfWork.Articles.GetAsync(a => a.Id == articleId, a => a.User, a => a.Category, a=>a.Comments);
             if (article != null)
             {
                 return new DataResult<ArticleDto>(ResultStatus.Success, new ArticleDto
