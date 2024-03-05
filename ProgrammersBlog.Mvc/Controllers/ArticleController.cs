25cab821826c1c0814d0af51b566103d38b14fc2
jox <zincir_890@hotmail.com>
2024-03-05 03:19:46 +0300

    Created IncreaseViewCount Operation for Articles

--- ProgrammersBlog.Mvc/Controllers/ArticleController.cs
+++ ProgrammersBlog.Mvc/Controllers/ArticleController.cs
@@ -18,7 +18,7 @@ namespace ProgrammersBlog.Mvc.Controllers
         public async Task<IActionResult> Search(string keyword, int currentPage = 1, int pageSize = 5, bool isAscending = false)
         {
             var searchResult = await _articleService.SearchAsync(keyword, currentPage, pageSize, isAscending);
-            if (searchResult.ResultStatus==ResultStatus.Success)
+            if (searchResult.ResultStatus == ResultStatus.Success)
             {
                 return View(new ArticleSearchViewModel
                 {
@@ -35,6 +35,7 @@ namespace ProgrammersBlog.Mvc.Controllers
             var articleResult = await _articleService.GetAsync(articleId);
             if (articleResult.ResultStatus == ResultStatus.Success)
             {
+                await _articleService.IncreaseViewCountAsync(articleId);
                 return View(articleResult.Data);
             }
 
