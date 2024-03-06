fd93794b035f82a7e52bab2ca4bd907b8cdea318
jox <zincir_890@hotmail.com>
2024-03-07 02:54:16 +0300

    Created ArticleDetail and ViewModel for SideBar as Class

--- ProgrammersBlog.Mvc/Controllers/ArticleController.cs
+++ ProgrammersBlog.Mvc/Controllers/ArticleController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Entities.ComplexTypes;
 using ProgrammersBlog.Mvc.Models;
 using ProgrammersBlog.Services.Abstract;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
@@ -35,8 +36,18 @@ namespace ProgrammersBlog.Mvc.Controllers
             var articleResult = await _articleService.GetAsync(articleId);
             if (articleResult.ResultStatus == ResultStatus.Success)
             {
+                var userArticles = await _articleService.GetAllByUserIdOnFilter(articleResult.Data.Article.UserId, FilterBy.Category, OrderBy.Date, false, 10, articleResult.Data.Article.CategoryId, DateTime.Now, DateTime.Now, 0, 99999, 0, 99999);
                 await _articleService.IncreaseViewCountAsync(articleId);
-                return View(articleResult.Data);
+                return View(new ArticleDetailViewModel
+                {
+                    ArticleDto = articleResult.Data,
+                    ArticleDetailRightSideBarViewModel = new ArticleDetailRightSideBarViewModel
+                    {
+                        ArticleListDto = userArticles.Data,
+                        Header = "Kullanıcının Aynı Kategori Üzerinde En Çok Okunan Makaleleri",
+                        User = articleResult.Data.Article.User
+                    }
+                });
             }
 
             return NotFound();
